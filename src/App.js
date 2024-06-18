fd20928f933e931891777def5d26c937897449d8
Vonister <ryansumague1030@gmail.com>
2024-06-18 12:30:07 +0800

    Vercel Statistics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import "react-date-range/dist/styles.css"; // main css file
-import "react-date-range/dist/theme/default.css"; // theme css file
-import { Route, Routes } from "react-router-dom";
-import Topbar from "./scenes/global/Topbar";
-import Pos from "./scenes/pos";
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { Route, Routes } from 'react-router-dom';
+import Topbar from './scenes/global/Topbar';
+import Pos from './scenes/pos';
 
-import themes from "./themes";
-import ToastCon from "./themes/toast";
+import themes from './themes';
+import ToastCon from './themes/toast';
+
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
@@ -25,6 +28,8 @@ function App() {
         </main>
         <ToastCon />
       </div>
+      <Analytics />
+      <SpeedInsights />
     </ThemeProvider>
   );
 }
